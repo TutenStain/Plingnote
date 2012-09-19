de2cf0671ca5fce9312a68e18abbe99cd2dd08a1
Bosch <magnushuttu@gmail.com>
2012-09-19 13:25:09 +0200

    We can now create taggs in the map
    We can create taggs in the map and they are shown with an image
    hovering above the tagged location. If the image is clicked/touched
    then there will be an overlay over the map which gives a small message.

--- Plingnote/src/com/plingbar/FragmentMapView.java
+++ Plingnote/src/com/plingbar/FragmentMapView.java
@@ -1,10 +1,16 @@
 package com.plingbar;
 
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.view.Menu;
 
 public class FragmentMapView extends MapActivity {
@@ -15,6 +21,9 @@ public class FragmentMapView extends MapActivity {
         setContentView(R.layout.activity_fragment_mapview);
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        List<Overlay> mapOverlays = mapView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+        ItemOverlay itemizedoverlay = new ItemOverlay(drawable, this);
     }
 
     @Override
