bb5fbd0cb2c8fd444d89b9c4e016d5b2416668de
Bosch <magnushuttu@gmail.com>
2012-09-20 16:34:18 +0200

    Deleted unnecessary files again!

--- src/com/plingbar/FragmentMapView.java
+++ src/com/plingbar/FragmentMapView.java
@@ -1,9 +1,11 @@
 package com.plingbar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
@@ -13,15 +15,30 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.view.Menu;
+import android.view.MotionEvent;
 
 public class FragmentMapView extends MapActivity {
 
-	@Override
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Drawable drawable;
+    private ItemOverlay itemizedoverlay;
+    List<Overlay> mapOverlays;
+    MapView mapView;
+    MapController mapController;
+
+    @Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_fragment_mapview);
-	    MapView mapView = (MapView) findViewById(R.id.mapview);
-	    mapView.setBuiltInZoomControls(true);
+	    mapView = (MapView) findViewById(R.id.mapview);
+	    mapOverlays = mapView.getOverlays();
+	    mapController = mapView.getController();
+
+		drawable = this.getResources().getDrawable(R.drawable.map_note);
+	    itemizedoverlay = new ItemOverlay(drawable, this);
+	    
+	    setMapAtStartUp(mapView);
+
 	}
 
     @Override
@@ -36,7 +53,16 @@ public class FragmentMapView extends MapActivity {
 	}
 	
 	private void setMapAtStartUp(MapView mapView){
-        mapView.setBuiltInZoomControls(true);
         mapView.setSatellite(true);
+        mapView.setBuiltInZoomControls(true);
+        mapController.setZoom(17);
+        mapController.setCenter(new GeoPoint(57706715, 11976419));
+	}
+	
+	public void createItemizedOverlay(GeoPoint point){
+	    OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+	    
+	    itemizedoverlay.addOverlay(overlayitem);
+	    mapOverlays.add(itemizedoverlay);
 	}
 }
