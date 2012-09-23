3400efae6b967b56a4bea33f0c4354efce70f2da
David Grankvist <starpie@saxycomp.(none)>
2012-09-23 14:48:41 +0200

    Class for creation and usage of the database and classes representing a note and its location on the map

---
+++ src/com/plingnote/Note.java
@@ -0,0 +1,53 @@
+package com.plingnote;
+
+public class Note {
+	private Integer rowId;
+	private String title;
+	private String text;
+	private Location location;
+
+	/**
+	 * 
+	 * @param id row id of this Note
+	 * @param ti title of this Note
+	 * @param txt text of this Note
+	 * @param l location of this Note
+	 */
+	public Note(Integer id, String ti, String txt, Location l){
+		this.rowId = id;
+		this.title = ti;
+		this.text = txt;
+		this.location = l;
+	}
+
+	/**
+	 * 
+	 * @return row id of this Note
+	 */
+	public Integer getRowId(){
+		return this.rowId;
+	}
+	/**
+	 * 
+	 * @return title of this Note
+	 */
+	public String getTitle(){
+		return this.title;
+	}
+
+	/**
+	 * 
+	 * @return text of this Note
+	 */
+	public String getText(){
+		return this.text;
+	}
+
+	/**
+	 * 
+	 * @return location of this Note
+	 */
+	public Location getLocation(){
+		return this.location;
+	}
+}
