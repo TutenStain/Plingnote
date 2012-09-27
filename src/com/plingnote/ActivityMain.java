b4d158ed90970c1489fa2488c989cb749cd4efe6
Bosch <magnushuttu@gmail.com>
2012-09-27 17:03:21 +0200

    Corrected a bug.

---
+++ src/com/plingnote/ActivityMain.java
@@ -0,0 +1,96 @@
+package com.plingnote;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabHost.TabContentFactory;
+import android.widget.TextView;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class ActivityMain extends MapActivity implements OnTabChangeListener{
+
+	private TabHost tabHost;
+	private ListView listView;
+	private MapView mapView;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+
+		setContentView(R.layout.activity_main);
+
+		tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		// setup must be called if you are not inflating the tabhost from XML
+		tabHost.setup();
+		tabHost.setOnTabChangedListener((OnTabChangeListener) this);
+
+		
+		// setup list view
+		listView = (ListView) findViewById(R.id.list);
+		listView.setEmptyView((TextView) findViewById(R.id.empty));
+
+		// create some dummy coordinates to add to the list
+		List<GeoPoint> pointsList = new ArrayList<GeoPoint>();
+		pointsList.add(new GeoPoint((int)(32.864*1E6), (int)(-117.2353*1E6)));
+		pointsList.add(new GeoPoint((int)(37.441*1E6), (int)(-122.1419*1E6)));
+		listView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, pointsList));
+		
+		// setup map view
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+		mapView.postInvalidate();
+		
+		// add views to tab host
+		tabHost.addTab(tabHost.newTabSpec("List").setIndicator("List").setContent(new TabContentFactory() {
+			public View createTabContent(String arg0) {
+				return listView;
+			}
+		}));
+		tabHost.addTab(tabHost.newTabSpec("Map").setIndicator("Map").setContent(new TabContentFactory() {
+			public View createTabContent(String arg0) {
+				return mapView;
+			}
+		}));
+		
+        tabHost.setCurrentTab(0);
+        tabHost.setCurrentTab(1);
+        
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		return true;
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
+	}
+
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onTabChanged(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
