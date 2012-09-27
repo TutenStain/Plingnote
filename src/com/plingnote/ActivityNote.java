7b0226772801ca4a2c466264e3f078eef8ba9366
Julia Gustafsson <godjulia@gmail.com>
2012-09-27 14:04:08 +0200

    Activitynote is now calling finish() if you press the back button

--- src/com/plingnote/ActivityNote.java
+++ src/com/plingnote/ActivityNote.java
@@ -61,7 +61,7 @@ public class ActivityNote extends FragmentActivity {
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 	    if (keyCode == KeyEvent.KEYCODE_BACK) {
-	        moveTaskToBack(true);
+	        finish();
 	        return true;
 	    }
 	    return super.onKeyDown(keyCode, event);
