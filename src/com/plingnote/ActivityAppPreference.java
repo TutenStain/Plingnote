513981621b7d7d37b2264091f33c31f637e47baa
Barnabas Sapan <barnabas.sapan@gmail.com>
2012-10-02 11:26:23 +0200

    Back button in actionbar.

--- src/com/plingnote/ActivityAppPreference.java
+++ src/com/plingnote/ActivityAppPreference.java
@@ -1,8 +1,10 @@
 package com.plingnote;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.view.MenuItem;
 
 /**
  * The activity that handles the settings menu
@@ -12,9 +14,24 @@ public class ActivityAppPreference extends PreferenceActivity{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
 		PreferenceManager prefManager = getPreferenceManager();
 		prefManager.setSharedPreferencesName("appPreferences");
 
 		addPreferencesFromResource(R.xml.app_preferences);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent intent = new Intent(this, ActivityMain.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
