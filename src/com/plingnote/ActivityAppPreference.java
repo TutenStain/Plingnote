47e254b7c2926ae1b1900aabbc6057d96d251912
Barnabas Sapan <barnabas.sapan@gmail.com>
2012-10-05 02:23:44 +0200

    Reworked settings that now uses fragments. Clear search history now also works.

--- src/com/plingnote/ActivityAppPreference.java
+++ src/com/plingnote/ActivityAppPreference.java
@@ -1,37 +1,19 @@
 package com.plingnote;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
-import android.view.MenuItem;
+
 
 /**
  * The activity that handles the settings menu
  */
-public class ActivityAppPreference extends PreferenceActivity{
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setTitle(R.string.menu_settings);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		PreferenceManager prefManager = getPreferenceManager();
-		prefManager.setSharedPreferencesName("appPreferences");
+public class ActivityAppPreference extends PreferenceActivity {	
 
-		addPreferencesFromResource(R.xml.app_preferences);
-	}
-	
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			Intent intent = new Intent(this, ActivityMain.class);
-			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivity(intent);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new FragmentAppPreference()).commit();
+    }
 }
