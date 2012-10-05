4296750540cd178419e25a7082e9d02c9ced043d
Barnabas Sapan <barnabas.sapan@gmail.com>
2012-10-05 11:41:34 +0200

    Added license.

--- src/com/plingnote/FragmentAppPreference.java
+++ src/com/plingnote/FragmentAppPreference.java
@@ -8,6 +8,24 @@ import android.preference.PreferenceFragment;
 import android.provider.SearchRecentSuggestions;
 
 /**
+ * This file is part of Plingnote.
+ * Copyright (C) 2012 Barnabas Sapan
+ * 
+ * Plingnote is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * The application preference fragment that shows the settings view.
  * This class listens to changes in the settings view so this
  * class is also responsible for responding to the user requested
