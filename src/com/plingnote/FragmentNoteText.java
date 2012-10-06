93fee741c8949ca89c7e632c8940df5fa1b9b9eb
Julia Gustafsson <godjulia@gmail.com>
2012-10-07 01:46:06 +0200

    Refactored the code about saving to database

--- src/com/plingnote/FragmentNoteText.java
+++ src/com/plingnote/FragmentNoteText.java
@@ -1,5 +1,7 @@
 package com.plingnote;
 
+
+
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Rect;
@@ -19,10 +21,13 @@ import android.widget.LinearLayout;
  */
 public class FragmentNoteText extends Fragment {
 
-
 	private View view;
 	private boolean isExisting = false;
-	private int id;
+	private int id = -1;
+
+	private Location location= null;
+	private String alarmString = "";
+	private String imagePath = "";
 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -62,7 +67,6 @@ public class FragmentNoteText extends Fragment {
 		noteText = (EditText) view.findViewById(R.id.notetext);
 		//If this class was opened with an intent or saved instances, the note text will get the text from the database
 		if(isExisting){
-
 			String txt = (DatabaseHandler.getInstance(this.getActivity()).getNote(this.id).getTitle());
 			txt = txt +(DatabaseHandler.getInstance(this.getActivity()).getNote(this.id).getText());	
 			//The cursor position will be saved even if turning the phone horizontal. Doesn't work with just setText or setSelection(noteText.getText().length()) if turning phone horizontal.
@@ -75,29 +79,27 @@ public class FragmentNoteText extends Fragment {
 		}
 	}
 
-	/**
-	 * Get the text from the view noteText. If it's during editing and is containing a title and/or a text the note's value will be updated in the database, if it's no title or text the note will be deleted in database. 
-	 * If not the note will be inserted in the database and the rowId will be saved. 
-	 */
 	@Override
 	public void onPause (){
 		super.onPause();
 		//If this class was opened with an intent or saved instance we are updating that note.
-		if(isExisting && (getTitleofNoteText().length() >0 || getTextofNoteText().length() > 0)){
-			DatabaseHandler.getInstance(this.getActivity()).updateNote(this.id,this.getTitleofNoteText(), this.getTextofNoteText(), null,null,null);
+		saveToDatabase();
+	}
+	/**
+	 * Get the text from the view noteText. If it's during editing and is containing a title and/or a text the note's value will be updated in the database.
+	 * If not the note will be inserted in the database and the rowId will be saved. 
+	 */
+	public void saveToDatabase(){
+		if(isExisting){
+			DatabaseHandler.getInstance(this.getActivity()).updateNote(this.id,this.getTitleofNoteText(), this.getTextofNoteText(), this.location,this.imagePath,this.alarmString);
 		}
 		//If this class not was opened with an intent o saved instance we are inserting the note in database.
 		else if(!isExisting){
-			if(getTitleofNoteText().length() >0 || getTextofNoteText().length() > 0){
-				DatabaseHandler.getInstance(this.getActivity()).insertNote(this.getTitleofNoteText(), this.getTextofNoteText(), null,null,null);
-				id = DatabaseHandler.getInstance(this.getActivity()).getLastId();
-				isExisting=true;
-			}
-		}else{
-			DatabaseHandler.getInstance(this.getActivity()).deleteNote(id);
+			DatabaseHandler.getInstance(this.getActivity()).insertNote(this.getTitleofNoteText(), this.getTextofNoteText(), this.location,this.imagePath,this.alarmString);
+			id = DatabaseHandler.getInstance(this.getActivity()).getLastId();
+			isExisting=true;
 		}
 	}
-
 	/**
 	 * Find the first line of the text in notetext in the layout and returning it as a string
 	 * @return
