9e116e470cf01c219c480f748166ce2b0531015b
Barnabas Sapan <barnabas.sapan@gmail.com>
2012-10-07 02:39:36 +0200

    On startup the map will get zoomed in to last known position. Find my position button implemented.

--- src/com/plingnote/ActivityMap.java
+++ src/com/plingnote/ActivityMap.java
@@ -1,37 +1,96 @@
-/*
- * Copyright (C) 2011 Ievgenii Nazaruk
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @modifiedBy: Barnabas Sapan
- */
 package com.plingnote;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import com.google.android.maps.MapActivity;
+import android.view.View;
 
-public class ActivityMap extends MapActivity {
-    
-    @Override
-    protected void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        setContentView(R.layout.fragment_mapview);
-    }
-    
-    @Override
-    protected boolean isRouteDisplayed() {
-        return false;
-    }
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+public class ActivityMap extends MapActivity implements LocationListener {
+	private MapController mc;
+	private MapView map;
+	private LocationManager locationManager;
+	private String provider;
+	private Location location;
+	private boolean isWantingAFix = false;
+	private Criteria criteria;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_mapview);
+		map = (MapView) findViewById(R.id.mapview);
+		mc = map.getController();
+
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		//Get the last known location from the network so we 
+		//quickly can zoom to the users position 
+		location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+	
+		//Zoom as soon as possible
+		zoomToCurrentPosition();		
+		
+		//Criteria for an accurate fix, presumably GPS
+		criteria = new Criteria();
+		criteria.setCostAllowed(true);
+		criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+
+	public void click(View view) {
+		isWantingAFix = true;
+				
+		provider = locationManager.getBestProvider(criteria, false);
+		locationManager.requestLocationUpdates(provider, 400, 1, this);
+		
+		zoomToCurrentPosition();
+	}
+	
+	private void zoomToCurrentPosition(){
+		mc.animateTo(new GeoPoint((int)(location.getLatitude() * 1E6), (int)(location.getLongitude() * 1E6)));
+		mc.setZoom(19);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if(isWantingAFix)
+			locationManager.requestLocationUpdates(provider, 400, 1, this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if(isWantingAFix)
+			locationManager.removeUpdates(this);
+	}
+
+
+	public void onLocationChanged(Location location) {
+		this.location = location;
+		
+		if(location.getAccuracy() >= 300){
+			locationManager.removeUpdates(this);
+		}
+	}
+
+	public void onProviderDisabled(String provider) { }
+
+	public void onProviderEnabled(String provider) { }
+
+	public void onStatusChanged(String provider, int status, Bundle extras) { }
 }
