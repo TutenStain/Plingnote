d3f61f6198d8b6cd9334f26794d31d08790317f9
Barnabas Sapan <barnabas.sapan@gmail.com>
2012-10-07 19:07:40 +0200

    Added an accuracy circle to the map.

--- src/com/plingnote/ActivityMap.java
+++ src/com/plingnote/ActivityMap.java
@@ -1,3 +1,20 @@
+/**
+ * This file is part of Plingnote.
+ * Copyright (C) 2012 Barnabas sapan
+ * 
+ * Plingnote is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.plingnote;
 
 import java.util.List;
@@ -16,8 +33,14 @@ import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
+/**
+ * The map activity
+ * @author Barnabas Sapan
+ */
 public class ActivityMap extends MapActivity implements LocationListener {
-	private final int DISABLE_GPS_WITH_ACCURACY_HIGHER_THEN = 200;
+	private final int DISABLE_GPS_WITH_ACCURACY_HIGHER_THEN = 100;
+	private final int GPS_SIGNIFICANTLY_BETTER_DATA_IF = 1000 * 60 * 2;
+	
 	private MapController mc;
 	private MapView map;
 	private LocationManager locationManager;
@@ -38,15 +61,15 @@ public class ActivityMap extends MapActivity implements LocationListener {
 		//Get the last known location from the network so we 
 		//quickly can zoom to the users position 
 		location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-	
+
 		//Zoom as soon as possible
 		zoomToCurrentPosition();		
-		
+
 		//Criteria for an accurate fix, presumably GPS
 		criteria = new Criteria();
 		criteria.setCostAllowed(true);
 		criteria.setAltitudeRequired(false);
-        criteria.setBearingRequired(false);
+		criteria.setBearingRequired(false);
 		criteria.setAccuracy(Criteria.ACCURACY_FINE);
 	}
 
@@ -57,21 +80,24 @@ public class ActivityMap extends MapActivity implements LocationListener {
 
 	public void click(View view) {
 		isWantingAFix = true;
-				
+
 		provider = locationManager.getBestProvider(criteria, false);
 		locationManager.requestLocationUpdates(provider, 400, 1, this);
-		
+
 		zoomToCurrentPosition();
 	}
-	
+
 	private void zoomToCurrentPosition(){
-		mc.animateTo(new GeoPoint((int)(location.getLatitude() * 1E6), (int)(location.getLongitude() * 1E6)));
-		
+		GeoPoint point = new GeoPoint((int)(location.getLatitude() * 1E6), (int)(location.getLongitude() * 1E6));
+		mc.animateTo(point);
+
 		List<Overlay> list = map.getOverlays();
 		list.clear();
-		list.add(new MapOverlayPin(this, map, new GeoPoint((int)(location.getLatitude() * 1E6), (int)(location.getLongitude() * 1E6))));
+		list.add(new MapOverlayPin(this, map, point));
+		list.add(new MapOverlayGPSAccuracy(point, location));
+
 		map.invalidate();
-		
+
 		mc.setZoom(19);
 	}
 
@@ -91,8 +117,11 @@ public class ActivityMap extends MapActivity implements LocationListener {
 
 
 	public void onLocationChanged(Location location) {
-		this.location = location;
-		
+		if (isBetterLocation(this.location, location)) {
+			this.location = location;
+			zoomToCurrentPosition();
+		}
+
 		//If we get a fix that is accurate enough, remove subscription from location updates
 		if(location.getAccuracy() <= DISABLE_GPS_WITH_ACCURACY_HIGHER_THEN){
 			locationManager.removeUpdates(this);
@@ -104,4 +133,60 @@ public class ActivityMap extends MapActivity implements LocationListener {
 	public void onProviderEnabled(String provider) { }
 
 	public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+	//Taken from Android Developers Guide examples 
+	//(http://developer.android.com/guide/topics/location/strategies.html)
+	/** Determines whether one Location reading is better than the current Location fix
+	 * @param location  The new Location that you want to evaluate
+	 * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+	 */
+	private boolean isBetterLocation(Location location, Location currentBestLocation) {
+		if (currentBestLocation == null) {
+			// A new location is always better than no location
+			return true;
+		}
+
+		// Check whether the new location fix is newer or older
+		long timeDelta = location.getTime() - currentBestLocation.getTime();
+		boolean isSignificantlyNewer = timeDelta > GPS_SIGNIFICANTLY_BETTER_DATA_IF;
+		boolean isSignificantlyOlder = timeDelta < -GPS_SIGNIFICANTLY_BETTER_DATA_IF;
+		boolean isNewer = timeDelta > 0;
+
+		// If it's been more than two minutes since the current location, use the new location
+		// because the user has likely moved
+		if (isSignificantlyNewer) {
+			return true;
+			// If the new location is more than two minutes older, it must be worse
+		} else if (isSignificantlyOlder) {
+			return false;
+		}
+
+		// Check whether the new location fix is more or less accurate
+		int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+		boolean isLessAccurate = accuracyDelta > 0;
+		boolean isMoreAccurate = accuracyDelta < 0;
+		boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+		// Check if the old and new location are from the same provider
+		boolean isFromSameProvider = isSameProvider(location.getProvider(),
+				currentBestLocation.getProvider());
+
+		// Determine location quality using a combination of timeliness and accuracy
+		if (isMoreAccurate) {
+			return true;
+		} else if (isNewer && !isLessAccurate) {
+			return true;
+		} else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+			return true;
+		}
+		return false;
+	}
+
+	/** Checks whether two providers are the same */
+	private boolean isSameProvider(String provider1, String provider2) {
+		if (provider1 == null) {
+			return provider2 == null;
+		}
+		return provider1.equals(provider2);
+	}
 }
