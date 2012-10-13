6c32683d1376c55d5b04041e202ceead81462c0f
Mushu <linus@mushu.it>
2012-10-13 22:17:27 +0200

    Add click listener to the grid view.

--- src/com/plingnote/SBCategorySelector.java
+++ src/com/plingnote/SBCategorySelector.java
@@ -4,8 +4,11 @@ import java.lang.reflect.Field;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -28,7 +31,41 @@ public class SBCategorySelector extends Fragment {
 
 		// Set adapter
 		categoryGrid.setAdapter(new CategoryAdapter());
+		categoryGrid.setOnItemClickListener(new OnItemClickListener() {
 
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				String clickedCategory = getCategoryDrawables()[position];
+				System.out.println(clickedCategory);
+			}
+		});
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.category_selector, container, false);
+
+	}
+
+	/**
+	 * Get an array with file names of all category images in drawable folder.
+	 * 
+	 * @return the file names of all catergories in drawable folder.
+	 */
+	public String[] getCategoryDrawables() {
+		Field[] fields = R.drawable.class.getFields();
+		String[] categoryDrawables = new String[fields.length];
+
+		// Place all category images in the array.
+		for (int i = 0; i < categoryDrawables.length; i++) {
+
+			// TODO compare names with category names in util.
+			categoryDrawables[i] = fields[i].getName();
+		}
+
+		return categoryDrawables;
 	}
 
 	/**
@@ -57,32 +94,15 @@ public class SBCategorySelector extends Fragment {
 				imageView = new ImageView(getActivity());
 			} else {
 				imageView = (ImageView) convertView;
+
+				imageView.setLayoutParams(new GridView.LayoutParams(150, 150));
+				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 			}
 
 			imageView.setImageResource(getActivity().getResources()
 					.getIdentifier(getCategoryDrawables()[position],
 							"drawable", "com.plingnote"));
 			return imageView;
-		}
-
-		/**
-		 * Get an array with file names of all category images in drawable
-		 * folder.
-		 * 
-		 * @return the file names of all catergories in drawable folder.
-		 */
-		public String[] getCategoryDrawables() {
-			Field[] fields = R.drawable.class.getFields();
-			String[] categoryDrawables = new String[fields.length];
-
-			// Place all category images in the array.
-			for (int i = 0; i < categoryDrawables.length; i++) {
-
-				// TODO compare names with category names in util.
-				categoryDrawables[i] = fields[i].getName();
-			}
-
-			return categoryDrawables;
 		}
 
 	}
