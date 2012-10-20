d30f496f42892c2013232d2184e7447cf533ac54
Barnabas Sapan <barnabas.sapan@gmail.com>
2012-10-21 00:57:26 +0200

    Removed a warning.

--- src/com/plingnote/SBImageSelector.java
+++ src/com/plingnote/SBImageSelector.java
@@ -41,6 +41,7 @@ import android.widget.GridView;
  */
 
 public class SBImageSelector extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+	@SuppressWarnings("unused")
 	private String selectedImage;
 	private SimpleCursorAdapter adapter;
 
