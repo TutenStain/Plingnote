344144862dc2d15f103b6c5ebd55652185a3d7a8
Bosch <magnushuttu@gmail.com>
2012-10-22 11:55:14 +0200

    Tests for gridview and mapview are now bugfree.

--- PlingnoteTest/src/com/plingnote/test/TestMapFunctions.java
+++ PlingnoteTest/src/com/plingnote/test/TestMapFunctions.java
@@ -3,12 +3,16 @@ package com.plingnote.test;
 import junit.framework.Assert;
 
 import com.plingnote.ActivityMain;
+import com.plingnote.Utils;
 import com.jayway.android.robotium.solo.Solo;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class TestMapFunctions extends
 ActivityInstrumentationTestCase2<ActivityMain> {
 	private Solo solo;
+	private int left = Utils.getScreenPixels(getActivity()).left;
+	private int right = Utils.getScreenPixels(getActivity()).right;
+	private int centerY = Utils.getScreenPixels(getActivity()).centerY();
 
 	public TestMapFunctions() {
 		super("com.plingnote", ActivityMain.class);
@@ -19,21 +23,21 @@ ActivityInstrumentationTestCase2<ActivityMain> {
 		super.setUp();
 		solo = new Solo(getInstrumentation(), getActivity());
 	}
-	
+
 	//Tests if the map will start
 	public void test1MapViewAtStartUp(){
 		solo.waitForView(null, ActivityMain.TabsAdapter.POSITION_UNCHANGED, 0);
 	}
-	
+
 	//Tests if the map is scrollable
 	public void test2MapScroll(){
-		solo.drag(100, 500, 500, 500, 20);
-		solo.drag(100, 500, 500, 500, 20);
-		solo.drag(100, 500, 500, 500, 20);
-		solo.drag(100, 500, 500, 500, 20);
+		solo.drag(left+50, right-50, 500, 500, 20);
+		solo.drag(left+50, right-50, 500, 500, 20);
+		solo.drag(left+50, right-50, 500, 500, 20);
+		solo.drag(left+50, right-50, 500, 500, 20);
 	}
-	
-	
+
+
 	//Tests if you can create a new note in mapview
 	public void test3CreateNewNote(){
 		solo.clickLongOnScreen(300, 500);
@@ -44,13 +48,24 @@ ActivityInstrumentationTestCase2<ActivityMain> {
 		solo.clickOnScreen(300, 500);
 		Assert.assertEquals("Yes this is dog", solo.getText(2).getEditableText().toString());
 	}
-	
+
 	//Tests if you can change tabs from mapview to another and then back.
 	public void test4ChangeViews(){
-		solo.drag(100, solo., 500, 500, 20);
-		solo.drag(719, 500, 500, 500, 20);
+
+		solo.drag(right-1, left+50 , centerY, centerY, 50);
+		solo.drag(left, right-50, centerY, centerY, 50);
 	}
-	
+
+	//Tests if you can edit an existing note
+	public void test5EditNote(){
+		solo.clickOnScreen(300, 500);
+		solo.clearEditText(1);
+		solo.enterText(1, "This text is edited");
+		solo.goBack();
+		solo.clickOnScreen(300, 500);
+		Assert.assertEquals("This text is edited", solo.getText(2).getEditableText().toString());
+	}
+
 	@Override
 	protected void tearDown() throws Exception {
 		solo.finishOpenedActivities();
