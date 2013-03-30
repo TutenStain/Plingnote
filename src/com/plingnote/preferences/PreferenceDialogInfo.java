7b9d1762db2f226084cef0eade1f1c2eeac9a7ad
Barnabas Sapan <barnabas.sapan@gmail.com>
2013-03-30 23:22:46 +0100

    Updated preferences buttons, looks better now

--- src/com/plingnote/preferences/PreferenceDialogInfo.java
+++ src/com/plingnote/preferences/PreferenceDialogInfo.java
@@ -17,52 +17,20 @@
 
 package com.plingnote.preferences;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.plingnote.R;
 
 /**
  * A custom class for displaying basic info with a
- * dismiss button. To be used in Preferences.
+ * dismiss button. To be used in Preferences. This is just 
+ * used to distinguish itself from the other preference classes,
+ * just calls super.
  * @author Barnabas Sapan
  */
 public class PreferenceDialogInfo extends DialogPreference{
-	private String dialogeMessage;
-	
 	public PreferenceDialogInfo(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		this.dialogeMessage = attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "dialogMessage");
-		setPersistent(false);
-		setDialogLayoutResource(R.layout.preference_dialog_info);
-	}
-	
-	@Override
-	protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
-		builder.setPositiveButton(null, null);
-		builder.setNegativeButton(null, null);
-		super.onPrepareDialogBuilder(builder);  
-	}
-	
-	@Override
-	public void onBindDialogView(View view){
-		Button okButon = (Button)view.findViewById(R.id.info_ok_button);
-		okButon.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				getDialog().dismiss();
-			}
-		});
-		
-		TextView textView = (TextView)view.findViewById(R.id.info_textview);
-		textView.setText(dialogeMessage);
-
-		super.onBindDialogView(view);
-	}
-
+        super(context, attrs);
+        setPersistent(false);
+    }
 }
