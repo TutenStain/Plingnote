86b53dc7c4ad86865901587816baf0e7980e81b0
Barnabas Sapan <barnabas.sapan@gmail.com>
2013-04-01 22:07:42 +0200

    Reworked reminder UI, not fully functional yet

--- src/com/plingnote/notifications/FragmentReminder.java
+++ src/com/plingnote/notifications/FragmentReminder.java
@@ -1,6 +1,6 @@
 /**
 * This file is part of Plingnote.
-* Copyright (C) 2012 Julia Gustafsson
+* Copyright (C) 2012 Julia Gustafsson, Barnabas Sapan
 *
 * Plingnote is free software: you can redistribute it and/or modify it under
 * the terms of the GNU General Public License as published by the Free Software
@@ -20,15 +20,19 @@ package com.plingnote.notifications;
 import java.util.Calendar;
 
 import android.app.AlarmManager;
+import android.app.DatePickerDialog;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TimePicker;
@@ -45,13 +49,16 @@ import com.plingnote.utils.NoteExtra;
 /**
  * A fragment representing a reminder/alarm that is using alarmmanager
  * @author Julia Gustafsson
+ * @Modifiedby Barnabas Sapan
  *
  */
-public class FragmentReminder extends Fragment implements PluginableFragment{
+public class FragmentReminder extends Fragment implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener, PluginableFragment{
 	private View view;
 	private String value = "";
 	private PendingIntent pendingIntent;
 	private int requestCode;
+	private boolean changedValues = false;
+	private int yearSet, monthSet, daySet, hourSet, minutesSet;
 
 	public View onCreateView(LayoutInflater inflater,
 			ViewGroup container, Bundle savedInstanceState) {
@@ -65,65 +72,104 @@ public class FragmentReminder extends Fragment implements PluginableFragment{
 	}
 	
 	/**
-	 * Set on click listers to the button
+	 * Attach onClick listeners to the buttons
 	 */
 	public void onStart(){
 		super.onStart();
+
+		final Button timeButton = (Button) this.view.findViewById(R.id.pick_time_button);
+		final Calendar c = Calendar.getInstance(); 
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		int minute = c.get(Calendar.MINUTE);
+		timeButton.setText(hour + ":" + minute);
+		timeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				DialogFragment newFragment = new FragmentTimePicker();
+			    newFragment.show(getChildFragmentManager(), "timePicker");
+			    changedValues = true;
+			}
+		});
+		
+		Button dateButton = (Button) this.view.findViewById(R.id.pick_date_button);
+		int year = c.get(Calendar.YEAR);
+		int month = c.get(Calendar.MONTH);
+		int day = c.get(Calendar.DAY_OF_MONTH);
+		dateButton.setText(year + "-" + month + "-" + day);
+		dateButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				 DialogFragment newFragment = new FragmentDatePicker();
+				 newFragment.show(getChildFragmentManager(), "datePicker");
+				 changedValues = true;
+			}
+		});
 		
 		ImageButton okey = (ImageButton) this.view.findViewById(R.id.ok);
 		ImageButton cancel = (ImageButton) this.view.findViewById(R.id.cancel);
 
 		cancel.setOnClickListener(new View.OnClickListener() {
-			
 			public void onClick(View v) {	  			
 				replaceBackFragment();
 			}
 		});	
 		
-		okey.setOnClickListener(new View.OnClickListener() {
-			
+		okey.setOnClickListener(new View.OnClickListener() {			
 			//Save the time and the the alarm in the method savetime
 			public void onClick(View v) {
-				saveTime(v);
+				if(changedValues)
+					saveTime();
+				else
+					Toast.makeText(getActivity(), "No reminder set, please change the time or date", Toast.LENGTH_LONG).show();
+				
 				replaceBackFragment();
 			}
 		});
 	}
+	
+	@Override
+	public void onDateSet(DatePicker view, int year, int month, int day) {
+		yearSet = year;
+		monthSet = month;
+		daySet = day;
+	}
 
+	@Override
+	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+		hourSet = hourOfDay;
+		minutesSet = minute;
+	}
+	
 	/**
 	 * Make an intent to broadcastreciever notenotification and save the time in datepicker and timepicker and set alarm by alarmmanager.
 	 * @param view
 	 */
-	public void saveTime(View view){
-		DatePicker datepicker = (DatePicker)this.view.findViewById(R.id.datePicker);
-		TimePicker  timepicker = (TimePicker)this.view.findViewById(R.id.timePicker);	
-		
+	public void saveTime(){
 		Intent intent = new Intent(getActivity(), NoteNotification.class);
 		ActivityNote activityNote = (ActivityNote)getActivity();
-		intent.putExtra(IntentExtra.id.toString(),activityNote.getId()); 	
+		intent.putExtra(IntentExtra.id.toString(), activityNote.getId()); 	
 		requestCode = DatabaseHandler.getInstance(getActivity()).getHighestRequest() + 1;
 		intent.putExtra(IntentExtra.requestCode.toString(),requestCode); 
 		pendingIntent = PendingIntent.getBroadcast(getActivity(), requestCode, intent, PendingIntent.FLAG_ONE_SHOT);
 		
-		Calendar calendar =  Calendar.getInstance();
-		calendar.set(datepicker.getYear(), datepicker.getMonth(),
-				datepicker.getDayOfMonth(), timepicker.getCurrentHour(), timepicker.getCurrentMinute(), 0);
-		this.value = calendar.getTime()+"";
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(yearSet, monthSet,	daySet, hourSet, minutesSet, 0);
+		this.value = calendar.getTime() + "";
 		
 		AlarmManager alarmManager = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
 		alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);    
 		Toast.makeText(getActivity(), "Reminder set : " + value, Toast.LENGTH_LONG).show();	
-		}
+	}
 
 	/**
-	 * Return the value of this appliation
+	 * Return the value of this application
 	 */
 	public String getValue() {
 		return this.value;
 	}
 
 	/**
-	 * Return the value of this appliation
+	 * Return the value of this application
 	 */
 	public int getRequestCode() {
 		return this.requestCode;
@@ -137,7 +183,7 @@ public class FragmentReminder extends Fragment implements PluginableFragment{
 	}
 
 	/**
-	 * Call replacebackfragment in the activity/fragment that host this fragment
+	 * Call replacebackfragment in the activity/fragment that hosts this fragment
 	 */
 	public void replaceBackFragment() {
 		ActivityNote activityNote = (ActivityNote)getActivity();
@@ -149,5 +195,5 @@ public class FragmentReminder extends Fragment implements PluginableFragment{
 	 */
 	public NoteCategory getCategory() {
 		return null;
-	}	 
+	}
 }
---
+++ src/com/plingnote/notifications/FragmentTimePicker.java
@@ -0,0 +1,24 @@
+package com.plingnote.notifications;
+
+import java.util.Calendar;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+
+public class FragmentTimePicker extends DialogFragment {
+		
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Use the current time as the default values for the picker
+		final Calendar c = Calendar.getInstance();
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		int minute = c.get(Calendar.MINUTE);
+
+		// Create a new instance of TimePickerDialog and return it
+		return new TimePickerDialog(getActivity(), (OnTimeSetListener) getParentFragment(), hour, minute, DateFormat.is24HourFormat(getActivity()));
+	}
+}
